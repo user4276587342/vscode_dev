c239ed84d50dab53b362edb783d060a998db8874
Damilola Faseun <damilola.faseun@gmail.com>
2024-10-03 22:14:03 +0100

    ionic react folder

---
+++ core/ionic-react/ionic-app/src/components/BmiResult.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { IonRow, IonCol, IonCard, IonCardContent } from '@ionic/react';
+
+const BmiResult: React.FC<{ result: number }> = props => {
+  return (
+    <IonRow>
+      <IonCol>
+        <IonCard>
+          <IonCardContent>
+            <h2 className='ion-text-center'>{props.result.toFixed(2)}</h2>
+          </IonCardContent>
+        </IonCard>
+      </IonCol>
+    </IonRow>
+  );
+};
+
+export default BmiResult;
